1016e6460d2ddeab2b096647bb1f1538e61c1e30
Santiago Soler <santiago.r.soler@gmail.com>
2018-09-25 20:07:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OneDark theme for xfce4-terminal
+
+Forked from [https://aur.archlinux.org/xfce-terminal-onedark.git]().
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+a="$(dirname $0)"
+cd "$a"
+dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
+echo "Copying .theme files to $dir ..."
+mkdir -p "$dir"
+cp -vin ./*.theme "$dir"
+echo "done."
+
+
+
