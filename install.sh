19933b613c4710780bc1125b4fec012b861bc64f
Santiago Soler <santiago.r.soler@gmail.com>
2018-09-25 20:31:39 -0300

    Moved themes to themes folder. Added onenotsodark.theme.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd "$a"
 dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
 echo "Copying .theme files to $dir ..."
 mkdir -p "$dir"
-cp -vin ./*.theme "$dir"
+cp -vin ./themes/*.theme "$dir"
 echo "done."
 
 
